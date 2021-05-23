193373e1c5eced1f54797c530f46cb6650ceb894
Julio-Maldonado <juliom@Julios-MacBook-Pro-3.local>
2021-05-23 18:14:47 -0500

    v2 of personal site

---
+++ src/Components/MediaPortfolio.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+
+class MediaPortfolio extends Component {
+  render() {
+    if (this.props.data) {
+      var media = this.props.data.media.map(function(media) {
+        var projectImage = 'images/media/'+media.image;
+        return (
+          <div key={media.title} className="columns media-item">
+            <div className="item-wrap">
+              <a href={media.url} title={media.title}>
+                <img alt={media.title} src={projectImage} />
+                <div className="overlay">
+                  <div className="media-item-meta">
+                    <h5>{media.title}</h5>
+                    <p>{media.category}</p>
+                  </div>
+                </div>
+                <div className="link-icon"><i className="fa fa-link"></i></div>
+              </a>
+            </div>
+          </div>
+        );
+      })
+    }
+
+    return (
+      <section id="media">
+        <div className="row">
+          <div className="twelve columns">
+            <h1>Featured By</h1>
+            <div id="media-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
+                {media}
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default MediaPortfolio;
---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+import About from './About';
+import Contact from './Contact';
+import Footer from './Footer';
+import Header from './Header';
+import MediaPortfolio from './MediaPortfolio';
+import Portfolio from './Portfolio';
+import Resume from './Resume';
+import Testimonials from './Testimonials';
+
+export {About, Contact, Footer, Header, MediaPortfolio, Portfolio, Resume, Testimonials};
